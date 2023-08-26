27314d81230a853189af4328511f86f95b5d3174
HaCsO <hacso@mail.ru>
2023-08-27 00:41:09 +0300

    daemon feature

--- bot.py
+++ bot.py
@@ -3,6 +3,7 @@ import imaplib
 import email
 from discord.ext import commands
 import configparser
+import daemon
 
 cfg = configparser.ConfigParser()
 cfg.read("config.cfg")
@@ -51,4 +52,5 @@ async def check_emails(ctx):
     await bot.get_channel(only_channel).send(f"`{finded}`" if finded else (f"`{dat[151][-11:-5]}`" if len(dat) else "Code not found!"))
     await ctx.respond(f"Смотри в <#{only_channel}>") # hardcoooode!!!
 
-bot.run(TOKEN)
+with daemon.DaemonContext():
+    bot.run(TOKEN)
